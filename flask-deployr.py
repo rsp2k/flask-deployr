13ca6162a7974e09db5dfac3bec03f30b1eea1e3
Ryan <deploy@collab-app.dcloud.cisco.com>
2017-03-26 00:17:15 -0400

    catchup

--- flask-deployr.py
+++ flask-deployr.py
@@ -10,16 +10,19 @@ import json
 import requests
 import ipaddress
 import hmac
+import shutil
 from hashlib import sha1
 
 import logging
 from logging.handlers import RotatingFileHandler
 
 import venv
+import configparser
 
 from flask import Flask, request, abort, jsonify
 
 from flask_admin import Admin
+from flask_admin.actions import action
 from flask_admin.contrib.sqla import ModelView
 
 from flask_sqlalchemy import SQLAlchemy
@@ -27,29 +30,25 @@ from sqlalchemy.ext.hybrid import hybrid_property
 
 CHECKOUT_BASE = '/home/deploy'
 VASSALS_DIR = '/etc/uwsgi/vassals'
-REPO_URL = 'https://github.com/%s.git'
-API_IPS_URL = 'https://api.github.com/meta'
+
+GITHUB_API_SERVERS = requests.get('https://api.github.com/meta').json()['hooks']
 GIT = '/usr/bin/git'
 
-
-
-
-import configparser
-
-def valid_signature(key, body, signature):
+def valid_request_signature(key, request):
     """
-    Validate signature is valid for body using key
+    Validate request was signed with key
     """
 
+    signature = request.headers.get('X-Hub-Signature')
     signature_parts = signature.split('=')
     if signature_parts[0] != "sha1":
         return False
-    generated_sig = hmac.new(str.encode(key), msg=body, digestmod=sha1)
+    generated_sig = hmac.new(str.encode(key), msg=request.data, digestmod=sha1)
     return hmac.compare_digest(generated_sig.hexdigest(), signature_parts[1])
 
 def check_request_ip(request):
     request_ip = ipaddress.ip_address(u'{0}'.format(request.remote_addr))
-    for block in requests.get(API_IPS_URL).json()['hooks']:
+    for block in GITHUB_API_SERVERS:
         if ipaddress.ip_address(request_ip) in ipaddress.ip_network(block):
             break
     else:
@@ -60,7 +59,6 @@ class ConfigClass(object):
     DEBUG = os.getenv('DEBUG', True)
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 
 # Setup Flask app and app.config
 app = Flask(__name__)
@@ -79,30 +77,22 @@ class EnvironmentVar(db.Model):
     applicaton = db.Column(db.String(255), db.ForeignKey('application.id'))
 
     def __repr__(self):
-        return "%s %s" % (self.application.name, self.name)
+        return "%s %s = %s" % (self.application.name, self.name, self.value)
 
 class Application(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    repo_name = db.Column(db.String(255), unique=True)
-    secret = db.Column(db.String(255), nullable=False, server_default='')
+    user = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+    repo_url = db.Column(db.String(255), unique=True, nullable=False)
+    webhook_secret = db.Column(db.String(255), nullable=False, server_default='')
     environment_variables = db.relationship('EnvironmentVar', backref='application', lazy='dynamic')
 
     def __repr__(self):
-        return self.name
-
-    @hybrid_property
-    def repo_url(self):
-        return REPO_URL % self.repo_name
+        return '<a href="%s">%s</a>' % (self.repo_url, self.name)
 
     @hybrid_property
     def path(self):
-        return os.path.join(CHECKOUT_BASE, self.repo_name)
-
-    @hybrid_property
-    def name(self):
-        if '/' in self.repo_name:
-           return self.repo_name.split('/')[1]
-        return self.repo_name
+        return os.path.join(CHECKOUT_BASE, self.name)
 
     def git_clone(self):
         """
@@ -154,7 +144,6 @@ class Application(db.Model):
         remove venv directory
         """
         if (os.path.exists(self.venv_path) and os.path.isdir(self.venv_path)):
-           import shutil
            shutil.rmtree(self.venv_path)
 
     def venv_create(self):
@@ -186,6 +175,10 @@ class Application(db.Model):
         return os.path.join(self.path, "%s.ini" % self.name)
 
     @hybrid_property
+    def uwsgi_vassal_symlink(self):
+       return os.path.join(VASSALS_DIR, os.path.basename(self.uwsgi_config_path))
+
+    @hybrid_property
     def uwsgi_config(self):
         """
         Return dictionary of uwsgi config
@@ -203,7 +196,7 @@ class Application(db.Model):
             'chmod-socket': '777',
 
             'plugins': 'python3,logfile',
-            'python-autoreload': '3',
+#            'python-autoreload': '3',
     
             'logger': logger_path,
             'req-logger': reqlog_path,
@@ -214,35 +207,80 @@ class Application(db.Model):
         """
         Write uwsgi config file for application
         """
+
+        self.envdir()
+
         config = configparser.ConfigParser(interpolation=None)
         config['uwsgi'] = self.uwsgi_config
-
-        print(self.uwsgi_config_path)
 
         with open(self.uwsgi_config_path, "w") as config_file:
             config.write(config_file)
  
 
-    def uwsgi_restart(self):
+    def start(self):
         """
-        Restart the uwsgi server
+        Start the uwsgi stie for this applicaton
         """
 
-        dst = os.path.join(VASSALS_DIR, os.path.basename(self.uwsgi_config_path))
+        self.uwsgi_write_config()
 
-        if (os.path.exists(dst) and os.path.islink(dst)):
-            from pathlib import Path
-            Path(self.uwsgi_config_path).touch()
+        if (os.path.exists(VASSALS_DIR) and os.path.isdir(VASSALS_DIR)):
+            dst = self.uwsgi_vassal_symlink
+            if os.path.islink(dst):
+                os.unlink(dst)
+            os.symlink(self.uwsgi_config_path, dst)
+            return True
 
-        elif (os.path.exists(VASSALS_DIR) and os.path.isdir(VASSALS_DIR)):
-            if not os.path.islink(dst):
-                os.symlink(self.uwsgi_config_path, dst)
+        raise Exception('Unable to write to vassals directory: %s' % dst)
+
+    def stop(self):
+        """
+        Stop the uwsgi site for this application
+        """
+
+        if os.path.islink(self.uwsgi_vassal_symlink):
+            os.unlink(self.uwsgi_vassal_symlink)
+            return True
+        return False
+
+    def restart(self):
+        self.stop()
+        self.start()
+
+    def delete_all_application_files(self):
+        """
+        Deletes files when application is deleted
+        """
+
+        self.stop()
+        if os.path.exists(self.path):
+            shutil.rmtree(self.path)
+
+        return True
 
 
 class ApplicationAdmin(ModelView):
-    column_display_pk = True
-    form_columns = ['repo_name', 'secret']
+#    column_display_pk = True
+#    form_columns = ['repo_url', 'name', 'webhook_secret']
     inline_models = (EnvironmentVar,)
+
+    def after_model_delete(self, model):
+        model.delete_all_application_files()
+
+    def after_model_change(self, form, model, is_created):
+        model.restart()
+
+    @action('pull', 'Pull', 'Are you sure you want to fetch/merge the selected applications?')
+    def action_pull(self, ids):
+        for id_ in ids:
+            a = Application.query.get(id_)
+            a.git_pull()
+
+    @action('restart', 'Restart', 'Are you sure you want to restart the selected applications?')
+    def action_restart(self, ids):
+        for id_ in ids:
+            a = Application.query.get(id_)
+            a.restart()
 
 
 app.config.update({
@@ -274,43 +312,27 @@ def webhook():
 
     payload = request.get_json()
 
-    repo_meta = {
-        'name': payload['repository']['name'],
-        'owner': payload['repository']['owner']['name'],
-    }
+    repo_name = payload['repository']['name']
 
-    application = None
-    match = re.match(r"refs/heads/(?P<branch>.*)", payload['ref'])
-    if match:
-        repo_meta['branch'] = match.groupdict()['branch']
-        repo_name = '{owner}/{name}/branch:{branch}'.format(**repo_meta)
-        application = Application.query.filter_by(repo_name=repo_name).first()
+    if repo_name is __name__:
+        return jsonify({'msg': "cant deploy repo named %s" % __name__})
 
-    # Fallback to plain owner/name lookup
-    if not application:
-        repo_name = '{owner}/{name}'.format(**repo_meta)
-        application = Application.query.filter_by(repo_name=repo_name).first()
-        if not application: abort(404)
+    application = Application.query.filter_by(name=repo_name).first()
+    if not application: abort(404)
 
-    if application.secret:
-        signature = request.headers.get('X-Hub-Signature')
-        if not valid_signature(application.secret, request.data, signature):
+    if application.webhook_secret:
+        if not valid_request_signature(application.webhook_secret, request):
             abort(400)
 
-    owner_root = os.path.join(CHECKOUT_BASE, repo_meta['owner'])
-    if not (os.path.exists(owner_root) and os.path.isdir(owner_root)):
-        os.makedirs(owner_root)
-
-    path = os.path.join(owner_root, repo_meta['name'])
+    path = os.path.join(CHECKOUT_BASE, application.name)
     if not (os.path.exists(path) and os.path.isdir(path)):
         application.git_clone()
         application.venv_create()
-        application.uwsgi_write_config()
     else:
         application.git_pull()
-        application.uwsgi_restart()
 
-    application.envdir()
+    application.pip_install_requirements()
+    application.restart()
 
     """
     actions = repo.get('action', None)
